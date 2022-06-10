7d56da60f1694bebc1421a823d536a0e91f638f3
Shinichiro Yabu <scin.ichi@gmail.com>
2022-06-10 13:40:50 +0900

    Update date concatination

--- lib/date.ts
+++ lib/date.ts
@@ -4,12 +4,10 @@ const TIMEZONE_OFFSET = 540;
 export const formatDate = (dateStr: string, format = "DATE_SHORT") => {
 	const date = new Date(dateStr);
 	date.setMinutes(date.getMinutes() + TIMEZONE_OFFSET);
-	return [
-		date.getUTCFullYear(),
-		'年',
-		date.getUTCMonth() + 1,
-		'月',
-		date.getUTCDate(),
-		'日',
-	].join('');
+	return date.getUTCFullYear()
+		+ '年'
+		+ (date.getUTCMonth() + 1)
+		+ '月'
+		+ date.getUTCDate()
+		+ '日'
 };
